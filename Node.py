b6ce8f32d7ca13c7c3b15eff4f02b0288e8f0819
vikram9x <vikrambakshi93@gmail.com>
2024-04-18 19:18:09 +0530

    Added 3 functions
    Added find_successor(), find_predecessor() and closest_preceding_finger()
    
    Edited hash()

--- Node.py
+++ Node.py
@@ -16,7 +16,24 @@ class Node(object):
         
     def hash(self,message):
         digest=hashlib.sha256(message.encode()).hexdigest()
-        digest=int(digest,16).pow(2,self.m)
+        digest=int(digest,16) % pow(2,self.m)
         
     def __str__(self):
         return str({"ip":self.ip,"port":self.port})
+
+    def find_successor(self, id):
+        p = self.find_predecessor(id)
+        return p.successor
+    
+    def find_predecessor(self, id):
+        n = self.id
+        while not(n < id <= n.successor):
+            n = n.closest_preceding_finger(id)
+        return n
+    
+    def closest_preceding_finger(self, id):
+        for i in range(self.m, 0, -1):
+            n = self.fingerTable.table[i-1][1]
+            if self.id < n.id < id:
+                return n
+        return self
